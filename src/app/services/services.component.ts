d54550862f7c1ecde1f7af37414f9496042252af
huysnake <105479758+huysnake@users.noreply.github.com>
2023-05-28 11:42:48 +0700

    edit sp

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -16,7 +16,12 @@ export class ProductService {
   getProductById(id: any): Observable<IProduct> {
     return this.http.get<IProduct>(`http://localhost:3000/products/${id}`)
   }
- 
+  addProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(`http://localhost:3000/products`,product)
+  }
+  updateProduct(product: IProduct): Observable<IProduct> {
+    return this.http.patch<IProduct>(`http://localhost:3000/products/${product.id}`,product)
+  }
   deleteProduct(id: any): Observable<IProduct> {
     return this.http.delete<IProduct>(`http://localhost:3000/products/${id}`);
   }
